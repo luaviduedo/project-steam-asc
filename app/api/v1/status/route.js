7092484b5bfb2cd1896664065beff1d05326680b
Lua Viduedo <luaviduedo@gmail.com>
2026-02-09 16:57:51 -0300

    add `database.js` and `.env.development` file

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,3 +1,8 @@
+import database from "../../../../infra/database.js";
+
 export async function GET() {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   return Response.json({ API: "Funcionando" }, { status: 200 });
 }
