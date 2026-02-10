226de1b39c45050409d35afdb762b5235e456eb1
Lua Viduedo <luaviduedo@gmail.com>
2026-02-10 10:49:10 -0300

    refactor GET test `api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,8 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  const responseBody = await response.json();
+
   expect(response.status).toBe(200);
+  expect(responseBody).toEqual({ API: "Funcionando" });
 });
